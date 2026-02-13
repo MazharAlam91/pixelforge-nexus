abb0fc43258f184a476409b340f733680ae825e7
Mazhar Alam <mansoorimazhar91@gmail.com>
2026-02-13 18:51:56 +0530

    minor update

--- server/server.js
+++ server/server.js
@@ -15,6 +15,10 @@ app.use(helmet({
   }));
 app.use(cors());
 app.use(express.json());
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "../public")));
+
 app.use(express.static("public"));
 
 
